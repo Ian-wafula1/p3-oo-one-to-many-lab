ba3ed39300077669a4c733611818f94fa85776ff
Ian Wafula <neocorpse@proton.me>
2025-05-14 19:48:36 +0300

    I'm awesome

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,40 @@
 class Pet:
-    pass
+    all = []
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    def __init__(self,name, pet_type, owner=None):
+        self.name = name
+        if pet_type not in self.PET_TYPES:
+            raise Exception()
+        else:
+            self.pet_type = pet_type
+        
+        if owner != None and not isinstance(owner, Owner):
+            raise Exception()
+        
+        self.owner = owner
+        Pet.all.append(self)
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+        else:
+            raise Exception()
+    
+    def get_sorted_pets(self):
+        pets = self.pets()
+        pets.sort(key=lambda x: x.name)
+        return pets
+        
+owner = Owner("John")
+pet1 = Pet("Fido", "dog", owner)
+pet2 = Pet("Clifford", "dog", owner)
+pet3 = Pet("Whiskers", "cat", owner)
+pet4 = Pet("Jerry", "reptile", owner)
+print(owner.get_sorted_pets())
